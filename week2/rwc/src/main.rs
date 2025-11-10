6ab092d9690aae1f6f94be2216188214334e40f7
Jinming Hu <438600480@qq.com>
2025-11-10 16:14:53 +0800

    week2 rwc done

--- week2/rwc/src/main.rs
+++ week2/rwc/src/main.rs
@@ -1,5 +1,19 @@
 use std::env;
 use std::process;
+use std::fs::File; // For read_file_lines()
+use std::io::{self, BufRead}; // For read_file_lines()
+
+/// Reads the file at the supplied path, and returns a vector of strings.
+fn read_file_lines(filename: &String) -> Result<Vec<String>, io::Error> {
+    let file = File::open(filename)?;
+    let mut res: Vec<String> = Vec::<String>::new();
+    for line in io::BufReader::new(file).lines() {
+        let line_str = line?;
+        res.push(line_str);
+    }
+    Ok(res)
+    // Be sure to delete the #[allow(unused)] line above
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,5 +22,12 @@ fn main() {
         process::exit(1);
     }
     let filename = &args[1];
-    // Your code here :)
+    let lines = read_file_lines(filename).unwrap();
+    let mut word_num = 0;
+    let mut char_num = 0;
+    for line in &lines {
+        word_num += line.split(' ').count();
+        char_num += line.chars().count();
+    }
+    println!("lines {}, words {}, chars {}", lines.len(), word_num, char_num);
 }
